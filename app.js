6eebbd27aa63c24880f31a967ea1791ac89c8dc1
xiangli <li.xiang@shopee.com>
2019-04-27 03:46:23 +0800

    init

---
+++ app.js
@@ -0,0 +1,72 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+var requestTime = function(req, res, next) {
+  req.requestTime = Date.now()
+  next()
+};
+
+app.use(requestTime);
+
+app.get('/time', function(req, res) {
+  var responseText = 'Hello World!<br>'
+  responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+  res.send(responseText)
+})
+
+app.all('/secret', function(req, res, next) {
+  console.log('Accessing the secret section ...')
+  next() // pass control to the next handler
+});
+app.get('/about', function(req, res) {
+  // res.send('GET request to the homepage');
+  res.render('index', { title: '这是新的title' });
+});
+app.get('/ab?cd', function(req, res) {
+  res.send('ab?cd')
+})
+app.get('/users/:userId/books/:bookId', (req, res) => {
+  res.send(req.params);
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  console.log(req);
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
