ca8ddcb62e31cf2c64c639d1dff84513f9d90b66
HealyXiang <936163033@qq.com>
2020-02-28 18:06:11 +0800

    add socket

--- socket/websocket.js
+++ socket/websocket.js
@@ -1,27 +1,34 @@
 // 服务端代码
-var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 8080});
 
-// 连接池
-var clients = [];
+var initWebsocket = function(server) {
+    var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer(server);
 
-wss.on('connection', function(ws) {
-    // 将该连接加入连接池
-    clients.push(ws);
-    ws.on('message', function(message) {
-        // 广播消息
-        clients.forEach(function(ws1){
-            if(ws1 !== ws) {
+    // 连接池
+    var clients = [];
+
+    wss.on('connection', function(ws) {
+        // 将该连接加入连接池
+        clients.push(ws);
+        ws.on('message', function(message) {
+            // 广播消息
+            // console.log('message', message);
+            clients.forEach(function(ws1){
+                // if(ws1 !== ws) {
+                //     ws1.send(message);
+                // }
                 ws1.send(message);
-            }
-      })
-    });
-
-    ws.on('close', function(message) {
-        // 连接关闭时，将其移出连接池
-        clients = clients.filter(function(ws1){
-            return ws1 !== ws
         })
+        });
+
+        ws.on('close', function(message) {
+            // 连接关闭时，将其移出连接池
+            clients = clients.filter(function(ws1){
+                return ws1 !== ws
+            })
+        });
     });
-});
-console.log('init socket!');
+    console.log('init socket!');
+}
+
+module.exports = initWebsocket;
